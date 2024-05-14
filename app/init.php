cb47b3840aabe8091999a008b3e8505fdae89f56
zulhusni2003 <zulhusnifamile@gmail.com>
2024-05-14 23:06:16 +0800

    Preparation

---
+++ app/init.php
@@ -0,0 +1,5 @@
+<?php 
+
+// Panggil class dlm folder core
+require_once 'core/App.php';
+require_once 'core/Controller.php';
---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php 
+
+require_once '../app/init.php';  // Ni akan panggil semua file MVC
+
+$app = new App;
