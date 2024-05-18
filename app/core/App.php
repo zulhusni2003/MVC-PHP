c1e9f36c012d283e78acb03a9a5f2db8586c601a
zulhusni2003 <zulhusnifamile@gmail.com>
2024-05-19 01:51:33 +0800

    Learn about controller

--- app/core/App.php
+++ app/core/App.php
@@ -1,12 +1,39 @@
 <?php 
 class App {
+    protected $controller = 'Home';
+    protected $method = 'index';
+    protected $params = [];
+    
     // The constructor method is automatically called when an instance of the App class is created.
     public function __construct()
     {
         // Call the parseURL method and store the result in the $url variable
         $url = $this->parseURL();
-        // Output the contents of the $url variable to the screen
-        var_dump($url);
+        
+        // Controller
+        if (!empty($url) && file_exists('../app/controllers/' . $url[0] . '.php')) {
+            $this->controller = $url[0];
+            unset($url[0]);
+        } 
+
+        require_once '../app/controllers/' . $this->controller . '.php';
+        $this->controller = new $this->controller;
+
+        // Method
+        if (isset($url[1])) {
+            if( method_exists($this->controller, $url[1])){
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+        }
+
+        // Params
+        if( !empty($url)){
+            $this->params = array_values($url);
+        }
+ 
+        // Jalankan method & controller, and send params if ada
+        call_user_func_array([$this->controller, $this->method], $this->params);
     }
 
     // Method to parse the URL from the GET request
