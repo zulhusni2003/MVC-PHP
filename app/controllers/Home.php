43ac6ba87b69c6ed3715fd85c9943f94f26a1f0b
zulhusni2003 <zulhusnifamile@gmail.com>
2024-05-19 07:59:15 +0800

    View

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -1,8 +1,14 @@
 <?php 
+require_once '../app/core/Controller.php';
 
-class Home {
+// Controller = Home, Method = index
+class Home extends Controller {
     public function index()
     {
-        echo "home/index";
+        // Call view method
+        $data['title'] = 'home';
+        $this->view('template/header', $data);
+        $this->view('home/index'); // folder view -> home -> index.php
+        $this->view('template/footer');
     }
 }
